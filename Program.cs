b1a53afb2c3928f21103bc754e27a39fb1cacfad
ifanzalukhu97 <ivan.zalukhu@simplidots.com>
2022-02-11 00:24:33 +0700

    Setup project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace health_checks_and_alerting
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+}
